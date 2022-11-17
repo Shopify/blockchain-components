6b701448f7a5efd43b17853165074fd67a8f0847
Quinton Chester <quinton.chester@shopify.com>
2022-11-17 17:32:28 -0600

    :sparkles: Adds theme customization functionality

--- packages/shared/src/providers/ThemeProvider.tsx
+++ packages/shared/src/providers/ThemeProvider.tsx
@@ -1,7 +1,19 @@
+import {FC} from 'react';
 import {ThemeProvider as StyledThemeProvider} from 'styled-components';
 
-import {Polaris} from '../themes';
+import {AvailableThemes} from '../themes';
+import {ThemeProps} from '../types/theme';
 
-export const ThemeProvider = ({children}: {children: any}) => {
-  return <StyledThemeProvider theme={Polaris}>{children}</StyledThemeProvider>;
+export const ThemeProvider: FC<ThemeProps> = ({
+  children,
+  customTheme,
+  theme = 'Polaris',
+}) => {
+  const providedTheme = AvailableThemes[theme];
+
+  return (
+    <StyledThemeProvider theme={customTheme || providedTheme}>
+      {children}
+    </StyledThemeProvider>
+  );
 };
--- packages/shared/src/themes/index.ts
+++ packages/shared/src/themes/index.ts
@@ -1,1 +1,21 @@
-export {Polaris} from './polaris';
+import {Dawn} from './dawn';
+import {Polaris} from './polaris';
+
+export const AvailableThemes = {
+  Dawn: Dawn,
+  Polaris: Polaris,
+};
+
+/**
+ * A type that represents the keys of themes defined in AvailableThemes.
+ *
+ * @example
+ * `Polaris`
+ */
+export type AvailableTheme = keyof typeof AvailableThemes;
+
+/**
+ * Export our themes so developers can inherit and override styles
+ * if they choose to do so.
+ */
+export {Dawn, Polaris};
