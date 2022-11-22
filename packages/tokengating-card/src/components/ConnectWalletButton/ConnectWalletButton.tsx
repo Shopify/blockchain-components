c7ec9e8558be79ff67cc5316fb19aa778f5f8b8e
Carolina Pinzon <carolina.pinzon@shopify.com>
2022-11-22 12:07:47 -0800

    Experiment button with class

--- packages/tokengating-card/src/components/ConnectWalletButton/ConnectWalletButton.tsx
+++ packages/tokengating-card/src/components/ConnectWalletButton/ConnectWalletButton.tsx
@@ -1,12 +1,20 @@
-import {ConnectWalletButtonStyle, ConnectWalletButtonText} from './style';
-
 interface ConnectWalletButtonProps {
   onConnectWallet: () => void;
 }
 
 const ConnectWalletButton = ({onConnectWallet}: ConnectWalletButtonProps) => (
-  <ConnectWalletButtonStyle id="connectWallet" onClick={onConnectWallet}>
-    <ConnectWalletButtonText>Connect wallet</ConnectWalletButtonText>
-  </ConnectWalletButtonStyle>
+  // Small test of a button that only uses classes.
+  // The commented version uses styled components.
+  // I'll keep it here so that we can easily move back to it
+  // <ConnectWalletButtonStyle id="connectWallet" onClick={onConnectWallet}>
+  //   <ConnectWalletButtonText>Connect wallet</ConnectWalletButtonText>
+  // </ConnectWalletButtonStyle>
+  <button
+    id="connectWallet"
+    onClick={onConnectWallet}
+    className="button button--full-width"
+  >
+    Connect wallet
+  </button>
 );
 export {ConnectWalletButton};
