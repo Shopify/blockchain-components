a97839841dc446bc8394970bbc6a141fa0addfa6
Quinton Chester <quinton.chester@shopify.com>
2022-11-30 20:26:26 -0600

    :construction_worker: Update build to ESM only for wallet-connection

--- packages/react-tokengating-app/vite.config.ts
+++ packages/react-tokengating-app/vite.config.ts
@@ -1,5 +1,5 @@
-import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
 // https://vitejs.dev/config/
@@ -7,8 +7,16 @@ export default defineConfig({
   plugins: [nodePolyfills(), react()],
   build: {
     rollupOptions: {
+      external: ['ethers', 'react', 'wagmi'],
       input: './src/index.tsx',
-      output: {entryFileNames: '[name].js'},
+      output: {
+        entryFileNames: '[name].js',
+        globals: {
+          ethers: 'ethers',
+          react: 'React',
+          wagmi: 'wagmi',
+        },
+      },
     },
   },
 });
