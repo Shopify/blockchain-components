bb986383c4e5be9d74db1a088c76ae9f9722ac4f
Quinton Chester <quinton.chester@shopify.com>
2022-12-05 13:11:40 -0600

    :sparkles: Adds Rainbow as a wallet choice in the connection modal

---
+++ packages/shared/assets/connectors/Rainbow.tsx
@@ -0,0 +1,170 @@
+export const Rainbow = (
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_6_19)">
+      <rect width="20" height="20" rx="4" fill="white" />
+      <g clipPath="url(#clip1_6_19)">
+        <path d="M20 0H0V20H20V0Z" fill="url(#paint0_linear_6_19)" />
+        <path
+          d="M3.33333 6.33333H4.33333C9.48798 6.33333 13.6667 10.512 13.6667 15.6667V16.6667H15.6667C16.2189 16.6667 16.6667 16.2189 16.6667 15.6667C16.6667 8.85515 11.1448 3.33333 4.33333 3.33333C3.78105 3.33333 3.33333 3.78105 3.33333 4.33333V6.33333Z"
+          fill="url(#paint1_radial_6_19)"
+        />
+        <path
+          d="M14 15.6667H16.6667C16.6667 16.219 16.219 16.6667 15.6667 16.6667H14V15.6667Z"
+          fill="url(#paint2_linear_6_19)"
+        />
+        <path
+          d="M4.33333 3.33333V6H3.33333V4.33333C3.33333 3.78105 3.78105 3.33333 4.33333 3.33333Z"
+          fill="url(#paint3_linear_6_19)"
+        />
+        <path
+          d="M3.33333 6H4.33333C9.67208 6 14 10.3279 14 15.6667V16.6667H11V15.6667C11 11.9848 8.01523 9 4.33333 9H3.33333V6Z"
+          fill="url(#paint4_radial_6_19)"
+        />
+        <path
+          d="M11.3333 15.6667H14V16.6667H11.3333V15.6667Z"
+          fill="url(#paint5_linear_6_19)"
+        />
+        <path
+          d="M3.33333 8.66667V6H4.33333V8.66667H3.33333Z"
+          fill="url(#paint6_linear_6_19)"
+        />
+        <path
+          d="M3.33333 10.3333C3.33333 10.8856 3.78105 11.3333 4.33333 11.3333C6.72657 11.3333 8.66667 13.2734 8.66667 15.6667C8.66667 16.219 9.11438 16.6667 9.66667 16.6667H11.3333V15.6667C11.3333 11.8007 8.19933 8.66667 4.33333 8.66667H3.33333V10.3333Z"
+          fill="url(#paint7_radial_6_19)"
+        />
+        <path
+          d="M8.66667 15.6667H11.3333V16.6667H9.66667C9.11438 16.6667 8.66667 16.219 8.66667 15.6667Z"
+          fill="url(#paint8_radial_6_19)"
+        />
+        <path
+          d="M4.33333 11.3333C3.78105 11.3333 3.33333 10.8856 3.33333 10.3333V8.66667H4.33333V11.3333Z"
+          fill="url(#paint9_radial_6_19)"
+        />
+      </g>
+    </g>
+    <defs>
+      <linearGradient
+        id="paint0_linear_6_19"
+        x1="10"
+        y1="0"
+        x2="10"
+        y2="20"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#174299" />
+        <stop offset="1" stopColor="#001E59" />
+      </linearGradient>
+      <radialGradient
+        id="paint1_radial_6_19"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(4.33333 15.6667) rotate(-90) scale(12.3333)"
+      >
+        <stop offset="0.770277" stopColor="#FF4000" />
+        <stop offset="1" stopColor="#8754C9" />
+      </radialGradient>
+      <linearGradient
+        id="paint2_linear_6_19"
+        x1="13.8333"
+        y1="16.1667"
+        x2="16.6667"
+        y2="16.1667"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#FF4000" />
+        <stop offset="1" stopColor="#8754C9" />
+      </linearGradient>
+      <linearGradient
+        id="paint3_linear_6_19"
+        x1="3.83333"
+        y1="3.33333"
+        x2="3.83333"
+        y2="6.16667"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#8754C9" />
+        <stop offset="1" stopColor="#FF4000" />
+      </linearGradient>
+      <radialGradient
+        id="paint4_radial_6_19"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(4.33333 15.6667) rotate(-90) scale(9.66667)"
+      >
+        <stop offset="0.723929" stopColor="#FFF700" />
+        <stop offset="1" stopColor="#FF9901" />
+      </radialGradient>
+      <linearGradient
+        id="paint5_linear_6_19"
+        x1="11.3333"
+        y1="16.1667"
+        x2="14"
+        y2="16.1667"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#FFF700" />
+        <stop offset="1" stopColor="#FF9901" />
+      </linearGradient>
+      <linearGradient
+        id="paint6_linear_6_19"
+        x1="3.83333"
+        y1="8.66667"
+        x2="3.83333"
+        y2="6"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#FFF700" />
+        <stop offset="1" stopColor="#FF9901" />
+      </linearGradient>
+      <radialGradient
+        id="paint7_radial_6_19"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(4.33333 15.6667) rotate(-90) scale(7)"
+      >
+        <stop offset="0.59513" stopColor="#00AAFF" />
+        <stop offset="1" stopColor="#01DA40" />
+      </radialGradient>
+      <radialGradient
+        id="paint8_radial_6_19"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(8.5 16.1667) scale(2.83333 7.55555)"
+      >
+        <stop stopColor="#00AAFF" />
+        <stop offset="1" stopColor="#01DA40" />
+      </radialGradient>
+      <radialGradient
+        id="paint9_radial_6_19"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(3.83333 11.5) rotate(-90) scale(2.83333 53.7283)"
+      >
+        <stop stopColor="#00AAFF" />
+        <stop offset="1" stopColor="#01DA40" />
+      </radialGradient>
+      <clipPath id="clip0_6_19">
+        <rect width="20" height="20" rx="4" fill="white" />
+      </clipPath>
+      <clipPath id="clip1_6_19">
+        <rect width="20" height="20" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
