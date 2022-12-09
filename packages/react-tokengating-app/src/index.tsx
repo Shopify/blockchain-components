c827303fbf89ab230285e9620e44f45ed9db920a
Carolina Pinzon <carolina.pinzon@shopify.com>
2022-12-09 10:58:02 -0500

    Create update function to ThemeAppExtension

--- packages/react-tokengating-app/src/index.tsx
+++ packages/react-tokengating-app/src/index.tsx
@@ -11,6 +11,30 @@ import {chains, client} from './wagmi';
 window.gmShop = {
   ThemeAppExtension: class ThemeAppExtension {
     private arguments;
+    private root: ReactDOM.Root | undefined;
+
+    private AppWithProviders({updatedProps}: {updatedProps?: any}) {
+      return (
+        <React.StrictMode>
+          <WagmiConfig client={client}>
+            <WalletConnectionProvider
+              chains={chains}
+              wallet={this.arguments?.initialState?.wallet || undefined}
+            >
+              <App
+                serverArguments={{
+                  ...this.arguments,
+                  initialState: {
+                    ...this.arguments.initialState,
+                    ...updatedProps,
+                  },
+                }}
+              />
+            </WalletConnectionProvider>
+          </WagmiConfig>
+        </React.StrictMode>
+      );
+    }
 
     constructor(args: any) {
       this.arguments = args;
@@ -20,23 +44,16 @@ window.gmShop = {
       const container = document.getElementById(this.arguments.containerId);
       if (!container) return;
 
-      const root = ReactDOM.createRoot(container as HTMLElement);
-      root.render(
-        <React.StrictMode>
-          <WagmiConfig client={client}>
-            <WalletConnectionProvider
-              chains={chains}
-              wallet={this.arguments?.initialState?.wallet || undefined}
-            >
-              <App serverArguments={this.arguments} />
-            </WalletConnectionProvider>
-          </WagmiConfig>
-        </React.StrictMode>,
-      );
+      this.root = ReactDOM.createRoot(container as HTMLElement);
+      this.root.render(this.AppWithProviders({}));
 
       // Setup event gm-shop-merchant-app event bus
       console.log('Event bus initialized');
       this.arguments?.setupEventBus?.(eventBus);
+    }
+
+    update(updatedProps: any) {
+      this.root?.render(this.AppWithProviders({updatedProps}));
     }
   },
 };
