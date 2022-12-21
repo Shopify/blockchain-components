efcb81fee7fd7c905b286c09b879b3b5ccfb9941
Quinton Chester <quinton.chester@shopify.com>
2022-12-21 17:01:39 -0600

    :globe_with_meridians: Adds internationalization to the WhatAreWallets screen

--- packages/connect-wallet/src/components/Modal/Screens/WhatAreWalletsScreen.tsx
+++ packages/connect-wallet/src/components/Modal/Screens/WhatAreWalletsScreen.tsx
@@ -1,11 +1,12 @@
+import {useI18n} from '@shopify/react-i18n';
 import {useCallback} from 'react';
-import {Button} from 'shared';
+import {Apps, Button, Gift, Key} from 'shared';
 
 import {Icon, ListItemContent, SheetContent, WalletListItem} from '../style';
-import {WhatAreWalletsInformation} from '../../../constants/what-are-wallets';
 import {ModalRoute, useModal} from '../../../providers/ModalProvider';
 
 const WhatAreWalletsScreen = () => {
+  const [i18n] = useI18n();
   const {navigation} = useModal();
 
   const handleGetAWallet = useCallback(() => {
@@ -14,19 +15,41 @@ const WhatAreWalletsScreen = () => {
 
   return (
     <SheetContent>
-      {WhatAreWalletsInformation.map(({content, icon, title}) => {
-        return (
-          <WalletListItem key={title}>
-            <Icon>{icon}</Icon>
+      <WalletListItem>
+        <Icon>{Apps}</Icon>
 
-            <ListItemContent>
-              <h3>{title}</h3>
+        <ListItemContent>
+          <h3>{i18n.translate('modalScreens.WhatAreWallets.home.title')}</h3>
 
-              <p>{content}</p>
-            </ListItemContent>
-          </WalletListItem>
-        );
-      })}
+          <p>{i18n.translate('modalScreens.WhatAreWallets.home.content')}</p>
+        </ListItemContent>
+      </WalletListItem>
+      <WalletListItem>
+        <Icon>{Key}</Icon>
+
+        <ListItemContent>
+          <h3>{i18n.translate('modalScreens.WhatAreWallets.login.title')}</h3>
+
+          <p>{i18n.translate('modalScreens.WhatAreWallets.login.content')}</p>
+        </ListItemContent>
+      </WalletListItem>
+      <WalletListItem>
+        <Icon>{Gift}</Icon>
+
+        <ListItemContent>
+          <h3>
+            {i18n.translate(
+              'modalScreens.WhatAreWallets.collaborativeCommerce.title',
+            )}
+          </h3>
+
+          <p>
+            {i18n.translate(
+              'modalScreens.WhatAreWallets.collaborativeCommerce.content',
+            )}
+          </p>
+        </ListItemContent>
+      </WalletListItem>
 
       <Button label="Get a wallet" onClick={handleGetAWallet} />
     </SheetContent>
