29d9114586bb0c7d034ab7ab3a919cc4cd410ea9
Quinton Chester <quinton.chester@shopify.com>
2023-01-09 17:22:56 -0600

    :label: Exports Theme type and Themes from packages

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -31,7 +31,7 @@ export {Dawn, Polaris} from './themes';
  * Types
  */
 export {type DeepPartial} from './types/deepPartial';
-export type {AvailableTheme, ThemeProps} from './types/theme';
+export type {AvailableTheme, Theme, ThemeProps} from './types/theme';
 
 /**
  * Utils
