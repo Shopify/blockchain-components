4d65269b1ae23962d52df976713eeb6b78632fb9
Quinton Chester <quinton.chester@shopify.com>
2023-01-17 17:37:27 -0600

    :bulb: Adds comments surrounding WC connectors in source

--- packages/connect-wallet/src/connectors/walletConnect.ts
+++ packages/connect-wallet/src/connectors/walletConnect.ts
@@ -20,9 +20,20 @@ export const WalletConnect = ({chains}: ConnectorProps): ConnectorInstance => {
   const standardConnector = buildWalletConnectConnector({
     chains,
     mobileLinks: supportedWalletConnectWallets,
-    qrcode: mobilePlatform !== undefined,
+    /**
+     * This prop disables/enables the WalletConnect modal usage.
+     * On desktop, we support connecting via WC via either scan
+     * or by using the WC modal. On mobile we only support
+     * connecting via the WC modal.
+     */
+    qrcode: Boolean(mobilePlatform),
   });
 
+  /**
+   * On mobile we need only a single connector, that's because
+   * on mobile the user cannot get to the scan screen. This means
+   * that users cannot select to open the modal connector.
+   */
   const modalConnector = mobilePlatform
     ? undefined
     : buildWalletConnectConnector({
