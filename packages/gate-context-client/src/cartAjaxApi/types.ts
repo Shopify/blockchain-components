a3be640ae0098c0e1f7017dda947d69d4b2ac61f
Jamie Ly <jamie.ly@shopify.com>
2023-01-23 13:45:53 -0500

    Stringify gate context before writing to cart ajax API

--- packages/gate-context-client/src/cartAjaxApi/types.ts
+++ packages/gate-context-client/src/cartAjaxApi/types.ts
@@ -1,8 +1,8 @@
-export interface CartAjaxAPICartAttributes<TGateContext> {
+export interface CartAjaxAPICartAttributes {
   'Wallet Address': string;
   // this is a special variable name that doesn't get copied
   // over to the order.
-  _shopify_gate_context?: TGateContext;
+  _shopify_gate_context?: string;
 }
 
 export interface ShopifyObject {
