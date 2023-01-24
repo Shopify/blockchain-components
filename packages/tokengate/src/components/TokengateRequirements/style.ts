1f964eef1a6e0e31eee27964793dbd9ad5ddd93b
Carolina Pinzon <carolina.pinzon@shopify.com>
2023-01-24 09:41:36 -0800

    remove top and bottom padding on segment condition list items

--- packages/tokengate/src/components/TokengateRequirements/style.ts
+++ packages/tokengate/src/components/TokengateRequirements/style.ts
@@ -4,7 +4,7 @@ export const TokengateRequirementsSeparatorStyle = styled.div<{$gap: string}>`
   display: flex;
   align-items: center;
   column-gap: ${({$gap}) => $gap};
-  padding: 4px 0;
+  margin: calc(${({theme}) => theme.typography.bodySm.fontSize} / -2) 0;
 
   &:before,
   &:after {
