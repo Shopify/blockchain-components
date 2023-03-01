23309b44420e7f4ec62eada99e580bc0ca8d8e0a
Leslie Xin <leslie.xin@shopify.com>
2023-03-01 00:23:55 -0700

    Adding test for buildConnectors

---
+++ packages/connect-wallet/src/connectors/buildConnectors.test.ts
@@ -0,0 +1,92 @@
+import {vi} from 'vitest';
+
+import type {Connector} from '../types/connector';
+import {CHAINS, CUSTOM_CONNECTOR} from '../test/fixtures/connector';
+
+import {buildConnectors} from './buildConnectors';
+
+const extractConnectorIds = (connectors: Connector[]) => {
+  return connectors.map((connector) => connector.id);
+};
+
+describe('buildConnectors', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe('when includeDefaults is true', () => {
+    it('returns list of default connectors', () => {
+      const {connectors, wagmiConnectors} = buildConnectors({
+        chains: CHAINS,
+        includeDefaults: true,
+      });
+
+      const connectorIds = extractConnectorIds(connectors);
+      const expectedConnectorsIds = [
+        'metaMask',
+        'coinbaseWallet',
+        'rainbow',
+        'ledger',
+        'walletConnect',
+      ];
+
+      expect(connectorIds).toEqual(expectedConnectorsIds);
+      expect(wagmiConnectors).toHaveLength(5);
+    });
+
+    it('returns list of filtered default connectors when excluded connectors are provided', () => {
+      const {connectors, wagmiConnectors} = buildConnectors({
+        chains: CHAINS,
+        excludedConnectors: ['metaMask'],
+        includeDefaults: true,
+      });
+
+      const connectorIds = extractConnectorIds(connectors);
+      const expectedConnectorsIds = [
+        'coinbaseWallet',
+        'rainbow',
+        'ledger',
+        'walletConnect',
+      ];
+
+      expect(connectorIds).toEqual(expectedConnectorsIds);
+      expect(wagmiConnectors).toHaveLength(4);
+    });
+
+    it('returns list of custom connectors and default connectors when custom connectors are provided', () => {
+      const {connectors, wagmiConnectors} = buildConnectors({
+        chains: CHAINS,
+        customConnectors: [CUSTOM_CONNECTOR],
+        includeDefaults: true,
+      });
+
+      const connectorIds = extractConnectorIds(connectors);
+      const expectedConnectorsIds = [
+        'customConnector',
+        'metaMask',
+        'coinbaseWallet',
+        'rainbow',
+        'ledger',
+        'walletConnect',
+      ];
+
+      expect(connectorIds).toEqual(expectedConnectorsIds);
+      expect(wagmiConnectors).toHaveLength(6);
+    });
+  });
+
+  describe('when includeDefaults is false', () => {
+    it('returns list of custom connectors', () => {
+      const {connectors, wagmiConnectors} = buildConnectors({
+        chains: CHAINS,
+        customConnectors: [CUSTOM_CONNECTOR],
+      });
+
+      const connectorIds = extractConnectorIds(connectors);
+      const expectedConnectorsIds = ['customConnector'];
+
+      expect(connectorIds).toEqual(expectedConnectorsIds);
+      expect(wagmiConnectors).toHaveLength(1);
+    });
+  });
+});
--- packages/connect-wallet/src/test/fixtures/connector.ts
+++ packages/connect-wallet/src/test/fixtures/connector.ts
@@ -1,7 +1,26 @@
+import {configureChains} from 'wagmi';
+import {InjectedConnector} from 'wagmi/connectors/injected';
+import {mainnet} from 'wagmi/chains';
+import {publicProvider} from 'wagmi/providers/public';
+
 import {SerializedConnector} from '../../types/connector';
 
 export const DEFAULT_SERIALIZED_CONNECTOR: SerializedConnector = {
   id: 'MetaMask',
   name: 'MetaMask',
   qrCodeSupported: false,
+};
+
+export const {chains: CHAINS} = configureChains([mainnet], [publicProvider()]);
+
+const INJECTED_CONNECTOR = new InjectedConnector({
+  chains: [mainnet],
+});
+
+export const CUSTOM_CONNECTOR = {
+  icon: null,
+  id: 'customConnector',
+  name: 'Custom Connector',
+  qrCodeSupported: false,
+  connector: INJECTED_CONNECTOR,
 };
