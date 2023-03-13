3af664ad9bebb32e69fc6460a7be14e366858d28
Quinton Chester <quinton.chester@shopify.com>
2023-03-13 17:25:47 -0500

    [Tailwind] tokengate/TokenListImage

--- packages/tokengate/src/components/TokenList/TokenListImage.tsx
+++ packages/tokengate/src/components/TokenList/TokenListImage.tsx
@@ -1,11 +1,14 @@
-import {SkeletonThumbnail} from 'shared';
-
-import {TokenListImageStyle} from './style';
-
 const TokenListImage = ({alt, imageUrl}: {alt: string; imageUrl?: string}) => {
   if (!imageUrl) {
-    return <SkeletonThumbnail />;
+    return <div className="sbc-h-full sbc-w-full sbc-bg-skeleton" />;
   }
-  return <TokenListImageStyle src={imageUrl} alt={alt} />;
+
+  return (
+    <img
+      alt={alt}
+      className="sbc-h-full sbc-w-full sbc-object-cover"
+      src={imageUrl}
+    />
+  );
 };
 export {TokenListImage};
