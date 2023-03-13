c05893a06c10a168609eef8f3121ff089db75de0
Quinton Chester <quinton.chester@shopify.com>
2023-03-13 16:03:53 -0500

    [Tailwind] tokengate/TokengateRequirementsSkeleton

--- packages/tokengate/src/components/TokengateRequirements/index.ts
+++ packages/tokengate/src/components/TokengateRequirements/index.ts
@@ -1,3 +1,2 @@
 export {TokengateRequirements} from './TokengateRequirements';
 export {TokengateRequirementsSeparator} from './TokengateRequirementsSeparator';
-export {TokengateRequirementsSkeleton} from './TokengateRequirementSkeleton';
