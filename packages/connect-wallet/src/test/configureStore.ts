ea5eed31fa0fc95e9b6e13519187c0932dca58ae
Quinton Chester <quinton.chester@shopify.com>
2023-05-19 22:43:01 -0500

    :construction: Remove references to Redux

--- packages/connect-wallet/src/test/configureStore.ts
+++ packages/connect-wallet/src/test/configureStore.ts
@@ -1,50 +1,50 @@
-import {configureStore} from '@reduxjs/toolkit';
+// import {configureStore} from '@reduxjs/toolkit';
 
-import logger from '~/middleware/loggerMiddleware';
-import {initialState as initialModalState} from '~/slices/modalSlice';
-import {initialState as initialWalletState} from '~/slices/walletSlice';
-import {rootReducer} from '~/store/combineReducers';
-import {listenerMiddleware} from '~/store/listenerMiddleware';
+// import logger from '~/state/middleware/loggerMiddleware';
+// import {initialState as initialModalState} from '~/slices/modalSlice';
+// import {initialState as initialWalletState} from '~/slices/walletSlice';
+// import {rootReducer} from '~/store/combineReducers';
+// import {listenerMiddleware} from '~/store/listenerMiddleware';
 
-export const preloadedState = {
-  modal: initialModalState,
-  wallet: initialWalletState,
-};
+// export const preloadedState = {
+//   modal: initialModalState,
+//   wallet: initialWalletState,
+// };
 
-export const store = configureStore({
-  reducer: rootReducer,
-  preloadedState,
-  middleware: (getDefaultMiddleware) => {
-    /**
-     * We prepend the listenerMiddleware here based on the comments
-     * in the middleware sample on the RTK docus.
-     *
-     * https://redux-toolkit.js.org/api/createListenerMiddleware#middleware
-     */
-    const middlewares = getDefaultMiddleware().prepend(
-      listenerMiddleware.middleware,
-    );
+// export const store = configureStore({
+//   reducer: rootReducer,
+//   preloadedState,
+//   middleware: (getDefaultMiddleware) => {
+//     /**
+//      * We prepend the listenerMiddleware here based on the comments
+//      * in the middleware sample on the RTK docus.
+//      *
+//      * https://redux-toolkit.js.org/api/createListenerMiddleware#middleware
+//      */
+//     const middlewares = getDefaultMiddleware().prepend(
+//       listenerMiddleware.middleware,
+//     );
 
-    return middlewares;
-  },
-});
+//     return middlewares;
+//   },
+// });
 
-export const storeWithLogger = configureStore({
-  reducer: rootReducer,
-  preloadedState,
-  middleware: (getDefaultMiddleware) => {
-    /**
-     * We prepend the listenerMiddleware here based on the comments
-     * in the middleware sample on the RTK docus.
-     *
-     * https://redux-toolkit.js.org/api/createListenerMiddleware#middleware
-     */
-    const middlewares = getDefaultMiddleware().prepend(
-      listenerMiddleware.middleware,
-    );
+// export const storeWithLogger = configureStore({
+//   reducer: rootReducer,
+//   preloadedState,
+//   middleware: (getDefaultMiddleware) => {
+//     /**
+//      * We prepend the listenerMiddleware here based on the comments
+//      * in the middleware sample on the RTK docus.
+//      *
+//      * https://redux-toolkit.js.org/api/createListenerMiddleware#middleware
+//      */
+//     const middlewares = getDefaultMiddleware().prepend(
+//       listenerMiddleware.middleware,
+//     );
 
-    middlewares.push(logger);
+//     middlewares.push(logger);
 
-    return middlewares;
-  },
-});
+//     return middlewares;
+//   },
+// });
---
+++ packages/connect-wallet/src/utils/attributeOrder.ts
@@ -0,0 +1,61 @@
+import {gateContextClient} from '../state/wallet/gateContextClient';
+
+import {OrderAttributionMode} from '~/types/orderAttribution';
+import {ConnectWalletError} from '~/utils/error';
+
+export interface AttributeOrderResponse {
+  orderAttributionMode: OrderAttributionMode;
+  wallet: {address: string; vaults?: string[]};
+}
+
+export async function attributeOrder({
+  wallet,
+  orderAttributionMode,
+}: AttributeOrderResponse) {
+  if (orderAttributionMode === 'disabled') {
+    return {
+      orderAttributionMode,
+    };
+  }
+
+  const {address, vaults} = wallet;
+
+  if (orderAttributionMode === 'ignoreErrors') {
+    gateContextClient
+      .write({
+        walletAddress: address,
+        vaults,
+      })
+      .catch((error: any) =>
+        console.error(
+          new ConnectWalletError(
+            'Error attributing order--ignoring due to orderAttributionMode=ignoreErrors',
+          ),
+          error,
+        ),
+      );
+
+    return {
+      orderAttributionMode,
+    };
+  }
+
+  if (!address) {
+    throw new ConnectWalletError('Missing payload during order attribution');
+  }
+
+  try {
+    await gateContextClient.write({
+      walletAddress: address,
+      vaults,
+    });
+
+    return {
+      orderAttributionMode,
+      address,
+      vaults,
+    };
+  } catch (error) {
+    throw new ConnectWalletError((error as any)?.message || error);
+  }
+}
