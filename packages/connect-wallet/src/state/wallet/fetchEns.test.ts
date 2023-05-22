ba5607609c8cbf221f9f01a69c88529a3530c5db
Quinton Chester <quinton.chester@shopify.com>
2023-05-22 11:40:49 -0500

    :white_check_mark: Add new testStore with Zustand and update fetchEns

--- packages/connect-wallet/src/state/wallet/fetchEns.test.ts
+++ packages/connect-wallet/src/state/wallet/fetchEns.test.ts
@@ -1,72 +1,61 @@
+import {act, renderHook} from '@testing-library/react';
 import {vi} from 'vitest';
 import {mainnet} from 'wagmi';
 
-import {fetchEns} from './fetchEns';
-import {addWallet} from './walletSlice';
-
-import {store} from '~/test/configureStore';
+import {FetchEnsProps} from '~/state/types';
 import {DEFAULT_WALLET} from '~/test/fixtures/wallet';
 import {mainnetPublicProvider} from '~/test/providers';
+import {useTestStore} from '~/test/store';
 import {Wallet} from '~/types/wallet';
 
 const defaultAddress = DEFAULT_WALLET.address;
 
-vi.mock('ethers', async () => {
-  const ethers = (await vi.importActual('ethers')) as any;
-
-  const lookupAddress = vi.fn((address: string) => {
-    if (address === '0x486D582eed105cEf4e4Aa270C93b1e03Fe5B04F3') {
+vi.mock('./fetchEns', () => {
+  const fetchEns = vi.fn((props: FetchEnsProps) => {
+    if (props.address === '0x486D582eed105cEf4e4Aa270C93b1e03Fe5B04F3') {
       return 'tobi.eth';
     }
 
     // Ignore any other test invocations.
-    return null;
+    return undefined;
   });
-
-  const resolveName = vi.fn((ensName: string) => {
-    if (ensName === 'tobi.eth') {
-      return '0x486D582eed105cEf4e4Aa270C93b1e03Fe5B04F3';
-    }
-
-    // Resolve to the default wallet address.
-    return '0x5ea9681C3Ab9B5739810F8b91aE65EC47de62119';
-  });
-
-  const providerOverrides = {
-    lookupAddress,
-    resolveName,
-  };
 
   return {
-    ...ethers,
-    providers: {
-      ...ethers.providers,
-      AlchemyProvider: vi.fn(() => providerOverrides),
-      FallbackProvider: vi.fn(() => providerOverrides),
-      StaticJsonRpcProvider: vi.fn(() => providerOverrides),
-    },
+    fetchEns,
   };
 });
 
 describe('fetchEns', () => {
+  const {result} = renderHook(() => useTestStore((state) => state.wallet));
+  const {addWallet, fetchEns} = result.current;
+
   afterEach(() => {
     vi.clearAllMocks();
+    const state = useTestStore.getState();
+
+    // Replace state after each test.
+    act(() => useTestStore.setState(state, true));
   });
 
   it('does not update the active wallet with a displayName value when an ensName is not resolved', async () => {
-    store.dispatch(addWallet(DEFAULT_WALLET));
+    // const mock = vi.fn().mockImplementationOnce()
+    act(() => addWallet(DEFAULT_WALLET));
 
-    expect(store.getState().wallet.activeWallet).toMatchObject(DEFAULT_WALLET);
+    expect(useTestStore.getState().wallet.activeWallet).toMatchObject(
+      DEFAULT_WALLET,
+    );
 
-    await store.dispatch(
-      fetchEns({
+    await act(async () => {
+      await fetchEns({
         address: defaultAddress,
         chain: mainnet,
         provider: mainnetPublicProvider,
-      }),
-    );
+      });
+    });
 
-    expect(store.getState().wallet.activeWallet).toMatchObject(DEFAULT_WALLET);
+    expect(useTestStore.getState().wallet.activeWallet).toMatchObject(
+      DEFAULT_WALLET,
+    );
   });
 
   it('updates the active wallet with a displayName value when an ensName is resolved', async () => {
@@ -75,26 +64,21 @@ describe('fetchEns', () => {
       address: '0x486D582eed105cEf4e4Aa270C93b1e03Fe5B04F3',
     };
 
-    store.dispatch(addWallet(testWallet));
+    act(() => addWallet(testWallet));
 
-    expect(store.getState().wallet.activeWallet).toMatchObject(testWallet);
+    expect(useTestStore.getState().wallet.activeWallet).toMatchObject(
+      testWallet,
+    );
 
-    const response = await store.dispatch(
-      fetchEns({
+    await act(async () => {
+      await fetchEns({
         address: '0x486D582eed105cEf4e4Aa270C93b1e03Fe5B04F3',
         chain: mainnet,
         provider: mainnetPublicProvider,
-      }),
-    );
-
-    expect(response).toMatchObject({
-      payload: {
-        address: '0x486D582eed105cEf4e4Aa270C93b1e03Fe5B04F3',
-        ensName: 'tobi.eth',
-      },
+      });
     });
 
-    expect(store.getState().wallet.activeWallet).toMatchObject({
+    expect(useTestStore.getState().wallet.activeWallet).toMatchObject({
       ...testWallet,
       displayName: 'tobi.eth',
     });
