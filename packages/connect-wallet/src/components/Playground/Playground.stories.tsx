56a06fb5d9e30a45fccf68022dab8143a5097c45
Quinton Chester <quinton.chester@shopify.com>
2023-06-26 14:49:00 -0400

    :green_heart: Update CI to include WalletConnect secret

--- packages/connect-wallet/src/components/Playground/Playground.stories.tsx
+++ packages/connect-wallet/src/components/Playground/Playground.stories.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @shopify/jsx-no-hardcoded-content */
 import type {Meta, StoryObj} from '@storybook/react';
+import {Text} from 'shared';
 import {WagmiConfig, configureChains, createConfig} from 'wagmi';
 import {mainnet} from 'wagmi/chains';
 import {publicProvider} from 'wagmi/providers/public';
@@ -9,17 +11,37 @@ import {buildConnectors} from '~/connectors/buildConnectors';
 import {ConnectWalletProvider} from '~/providers/ConnectWalletProvider';
 
 interface TemplateProps {
-  wallets: 'Ethereum' | 'Solana';
   connectScreenHeader?: string;
+  walletConnectProjectId?: string;
+  wallets: 'Ethereum' | 'Solana';
 }
 
-const Component = ({wallets, connectScreenHeader}: TemplateProps) => {
+const Component = ({
+  connectScreenHeader,
+  walletConnectProjectId,
+  wallets,
+}: TemplateProps) => {
   const chains = wallets === 'Ethereum' ? [mainnet] : [];
   const {publicClient, webSocketPublicClient} = configureChains(chains, [
     publicProvider(),
   ]);
 
-  const {connectors, wagmiConnectors} = buildConnectors({chains});
+  if (!walletConnectProjectId) {
+    return (
+      <Text>
+        No WalletConnect{' '}
+        <code className="inline-block sbc-border sbc-rounded-button-small sbc-bg-button-secondary-hover sbc-px-1 sbc-py-1 sbc-border-button-secondary">
+          projectId
+        </code>{' '}
+        provided
+      </Text>
+    );
+  }
+
+  const {connectors, wagmiConnectors} = buildConnectors({
+    chains,
+    projectId: walletConnectProjectId,
+  });
 
   const client = createConfig({
     autoConnect: true,
@@ -50,10 +72,17 @@ type Story = StoryObj<TemplateProps>;
 
 export const Playground: Story = {
   args: {
-    wallets: 'Ethereum',
     connectScreenHeader: 'Connect Wallet',
+    walletConnectProjectId: import.meta.env.STORYBOOK_WALLET_CONNECT_PROJECT_ID,
+    wallets: 'Ethereum',
   },
   argTypes: {
+    walletConnectProjectId: {
+      table: {
+        disable: true,
+      },
+      // disable: true,
+    },
     wallets: {
       control: 'select',
       options: ['Ethereum'],
